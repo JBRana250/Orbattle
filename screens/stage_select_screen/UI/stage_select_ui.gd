1bcfdfc1ec9959d3968e1d04f15b844dfda06765
JB Rana <JBRana2500@gmail.com>
2025-08-01 13:48:28 +1200

    animated stage select ui transparancy and reset ease type

--- screens/stage_select_screen/UI/stage_select_ui.gd
+++ screens/stage_select_screen/UI/stage_select_ui.gd
@@ -9,13 +9,18 @@ extends Node
 # Region Markers will add and remove themselves to this array.
 @export var current_region_markers: Array[Node]
 
+@export var fully_transparent_modulate: Color
+@export var fully_opaque_modulate: Color
+
 # This function NEEDS to be called whenever the above array is modified
 func on_array_modified():
 	if current_region_markers.is_empty():
 		self.visible = false
+		self.modulate = fully_transparent_modulate
 		zoom_out_to_full()
 	else:
 		self.visible = true
+		_start_tween(self, "modulate", fully_opaque_modulate, tween_duration)
 
 func zoom_out_to_full():
 	_start_tween(camera, "position", default_position, tween_duration)
@@ -23,4 +28,5 @@ func zoom_out_to_full():
 	
 func _start_tween(object: Object, property: String, final_val: Variant, duration: float):
 	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(object, property, final_val, duration)
